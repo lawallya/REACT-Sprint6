a34a592639df5a4aa97a4b5a118049c534719051
Alejandra Cabello <alejandracabellomunoz@gmail.com>
2022-11-24 12:32:39 +0100

    Ejercicio 1

---
+++ src/componentes/escenas/Escena.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+/**EJERCICIO 1 */
+export const Escena = () => <p>"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p>;
